15d0a96982b52e2d029a6d697cd91bcb8cdc3276
Giovanni Tonussi <itm_gtonussi@uolinc.com>
2022-05-31 21:39:11 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,7 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+import theme from '../styles/theme'
+import GlobalStyle from '../styles/global'
 
 export default function App({ Component, pageProps }) {
   return (
