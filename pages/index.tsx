21b5319c9265701c342ea0b0f36d8f7fe23d206c
Giovanni Tonussi <itm_gtonussi@uolinc.com>
2022-05-31 21:51:22 -0300

    feat(aos): install and initialize

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
+
 import * as S from '../styles/pages/home'
 
 export default function Home() {
-  return <S.Title>My page</S.Title>
+  useEffect(() => {
+    AOS.init()
+  })
+
+  return <S.Title data-aos="fade-up">My page</S.Title>
 }
