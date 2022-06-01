7a02d5dd94d65f283a94c807d2d2af5f9a6450c8
Giovanni Tonussi <itm_gtonussi@uolinc.com>
2022-05-31 21:42:51 -0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,5 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import * as S from '../styles/pages/home'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <S.Title>My page</S.Title>
 }
