f8be567980f47aa0ecc20288f950db209a9d2128
Giovanni Tonussi <itm_gtonussi@uolinc.com>
2022-06-06 10:59:32 -0300

    fix(whatsappbutton): remove the need for react-responsive creating a custom hook

--- components/WhatsappButton/WhatsappButton.tsx
+++ components/WhatsappButton/WhatsappButton.tsx
@@ -1,21 +1,19 @@
-import { useMediaQuery } from 'react-responsive'
 import { ImWhatsapp } from 'react-icons/im'
 import { RiWhatsappFill } from 'react-icons/ri'
 
 import { Button } from '../Button'
+import { useWindowSize } from '../../hooks/useWindowSize'
 
 import * as S from './WhatsappButton.styles'
 
 export const WhatsappButton = () => {
-  const is400 = useMediaQuery({
-    query: '(min-width: 400px)',
-  })
+  const { width } = useWindowSize()
 
   return (
     <S.WhatsappButton__Container>
-      <Button size={is400 ? 'xlarge' : 'large'} variation="whatsapp">
-        ENTRE EM {!is400 && <br />} CONTATO AGORA!
-        <span style={{ paddingRight: is400 ? '40px' : '28px' }}></span>
+      <Button size={width > 400 ? 'xlarge' : 'large'} variation="whatsapp">
+        ENTRE EM {!(width > 400) && <br />} CONTATO AGORA!
+        <span style={{ paddingRight: width > 400 ? '40px' : '28px' }}></span>
       </Button>
       <S.WhatsappButton__IconsWrapper>
         <ImWhatsapp />
